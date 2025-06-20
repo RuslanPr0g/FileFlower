5e86f6c3df8840d6e6b4e76f1622716fc23ed7cf
Ruslan Rudenko <theruslan.prog@gmail.com>
2025-06-20 16:09:30 +0200

    Sample Console Fix

--- src/FileFlower.Sample.Console/Program.cs
+++ src/FileFlower.Sample.Console/Program.cs
@@ -1,15 +1,31 @@
-﻿using FileFlower.Core.FileWatchers;
+﻿using FileFlower.Core.Extensions;
+using FileFlower.Core.FileWatchers;
 
 string currentDirectory = Environment.CurrentDirectory;
 Console.WriteLine("Current directory: " + currentDirectory);
 
-var fileWatcher = new FileWatcherBuilder("~/files")
-    .Filter("*.txt")
-    .AddStep(Log)
-    .Start();
+var watcher = new FileWatcherBuilder("~/files");
 
-static async Task Log(FileInfo file) => Console.WriteLine($"called FOR FILE WATCHER subscription!! {file.FullName}");
+watcher.WhenResourceCreated(rule => rule.Filter("*.txt").Filter("*.csv").WithOrLogic())
+       .AddStep(file =>
+       {
+           Console.WriteLine($"CLIENT | Processed txt or csv: {file.FullName}");
+           return Task.CompletedTask;
+       });
+
+watcher.WhenResourceCreated(rule => rule.Filter("*test*").Filter("*.bat"))
+       .AddStep(file =>
+       {
+           Console.WriteLine($"CLIENT | Processed bat which contains word 'test': {file.FullName}");
+           return Task.CompletedTask;
+       });
+
+watcher.WhenResourceCreated(rule => rule.Filter("*.log"))
+       .AddStep(file =>
+       {
+           Console.WriteLine($"CLIENT | Processed log: {file.FullName}");
+           return Task.CompletedTask;
+       });
 
 Console.WriteLine("Please wait for file changes in: " + currentDirectory);
-
 Console.ReadLine();
