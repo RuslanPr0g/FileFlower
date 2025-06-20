9cbf14ed0faa17e38a52a15dc235cb0a4724c6bc
Ruslan Rudenko <theruslan.prog@gmail.com>
2025-06-20 14:27:32 +0200

    leave only essentials

--- src/FileFlower.Sample.Hosted.WebApi/Program.cs
+++ src/FileFlower.Sample.Hosted.WebApi/Program.cs
@@ -2,8 +2,6 @@ using FileFlower.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOpenApi();
-
 builder.Services.AddFileWatcher(@"./incoming/", watcher =>
 {
     watcher.Filter("*.txt")
@@ -16,35 +14,6 @@ builder.Services.AddFileWatcher(@"./incoming/", watcher =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
